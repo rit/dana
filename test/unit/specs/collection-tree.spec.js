f6412dc6e44d8fe5f526a024a83da7f0a6fc3a57
Rit Li <get@rit.li>
2017-05-09 23:29:29 -0700

    Clean up spec

--- test/unit/specs/collection-tree.spec.js
+++ test/unit/specs/collection-tree.spec.js
@@ -3,8 +3,11 @@ import ElementUI from 'element-ui'
 import CollectionTree from '@/components/collection-tree'
 
 describe('Collection Tree', () => {
-  it('acts like an accordion', () => {
+  beforeEach(() => {
     Vue.use(ElementUI)
+  });
+
+  it('acts like an accordion', () => {
     const vm = new Vue(CollectionTree).$mount()
     expect(vm.$el.textContent).to.contain('Level')
   });
