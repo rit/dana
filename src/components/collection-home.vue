3633c307703c19c4ba32ce90635f96c339bb3581
Rit Li <get@rit.li>
2017-05-12 14:26:14 -0700

    Get collection id from the parent component

--- src/components/collection-home.vue
+++ src/components/collection-home.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
   <h2>Collection Heading - {{ accessionNo }} </h2>
-  <collection-heading></collection-heading>
-  <series-heading></series-heading>
+  <!--<collection-heading></collection-heading>-->
+  <!--<series-heading></series-heading>-->
   <section class="main">
-    <series-tree></series-tree>
-    <section class="content">
-      <content-heading></content-heading>
-      <content-tree></content-tree>
-    </section>
+    <series-tree :collectionSlug="accessionNo" ></series-tree>
+    <!--<section class="content">-->
+    <!--  <content-heading></content-heading>-->
+    <!--  <content-tree></content-tree>-->
+    <!--</section>-->
   </section>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import CollectionHome from '@/components/collection-home'
+import SeriesTree from '@/components/series-tree'
 
 Vue.use(Router)
+Vue.component('series-tree', SeriesTree)
 
 export default new Router({
   routes: [
