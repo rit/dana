225914507c301d5e1a9f356403fa19cbd3b02488
Rit Li <get@rit.li>
2017-05-17 11:13:16 -0700

    Use Element's grid layout

--- src/ui/components/layout.vue
+++ src/ui/components/layout.vue
@@ -1,7 +1,26 @@
 <template>
   <div>
 
-    <h1>{{ greeting }}, world!</h1>
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content bg-purple-dark">
+          <h1>{{ greeting }}, world!</h1>
+        </div>
+      </el-col>
+    </el-row>
+
+    <el-row>
+      <el-col :span="12">
+        <div class="grid-content bg-purple">
+          left content
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div class="grid-content bg-purple-light">
+          right content
+        </div>
+      </el-col>
+    </el-row>
 
   </div>
 </template>
