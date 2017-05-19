a8f27d5dae9c252dbb6cb20abc4421cb9ce7f66c
Rit Li <get@rit.li>
2017-05-18 18:15:52 -0700

    Show summary or content tree

--- src/ui/components/collection-content.vue
+++ src/ui/components/collection-content.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
-    <h1>Collection Content</h1>
+    <!--<collection-heading :slug="lastSlug"></collection-heading>-->
+    <div v-if="showSummary">
+      <collection-summary></collection-summary>
+    </div>
+    <div v-else>
+      <collection-content-tree></collection-content-tree>
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
+
+  props: ['collectionSlug', 'seriesSlug'],
+
+  computed: {
+    lastSlug () {
+      return this.seriesSlug || this.collectionSlug
+    },
+
+    showSummary () {
+      // TODO What constitute a leaf collection?
+      return this.seriesSlug !== 'sample'
+    }
+  },
+
   created () {
     console.log('content component created')
   }
--- src/ui/router/index.js
+++ src/ui/router/index.js
@@ -6,10 +6,14 @@ import CollectionHome from '@ui/components/collection-home'
 import CollectionHeading from '@ui/components/collection-heading'
 import CollectionContent from '@ui/components/collection-content'
 import SeriesTree from '@ui/components/series-tree'
+import CollectionSummary from '@ui/components/collection-summary'
+import CollectionContentTree from '@ui/components/collection-content-tree'
 
 Vue.use(Router)
 
 Vue.component('collection-content', CollectionContent)
+Vue.component('collection-content-tree', CollectionContentTree)
+Vue.component('collection-summary', CollectionSummary)
 Vue.component('collection-layout', CollectionLayout)
 Vue.component('collection-heading', CollectionHeading)
 Vue.component('series-tree', SeriesTree)
@@ -35,7 +39,7 @@ export default new Router({
           path: 'series/:seriesSlug',
           props: true,
           name: 'SeriesHome',
-          component: CollectionHome
+          component: CollectionContent
         }
       ]
     }
