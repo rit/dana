3ebccaf61a86a8bdad727f7b750fbcb30218c7d0
Rit Li <get@rit.li>
2017-05-21 12:06:29 -0700

    Show hot reload counter

--- test/unit/mocha-ended-callback.js
+++ test/unit/mocha-ended-callback.js
@@ -1,8 +1,16 @@
+var $ = require('jquery')
 var n = 1
 
 function ended () {
   const counter = () => n++
-  console.log(`Mocha Completed #: ${counter()}`);
+
+  var div = $('#mocha')
+  var hot = $('#hot')
+  if (!hot.length) {
+    hot = $('<div>', { id: 'hot', style: 'position: fixed; left: 3em; top: 1em' })
+  }
+  hot.html(`Hot Reload: ${counter()}`)
+  hot.prependTo(div)
 }
 
 global.mochaEndedCallback = ended
