fff090d7adc910f2cb186184b9d3c8be53c0ef22
Rit Li <get@rit.li>
2017-06-07 22:25:21 -0700

    Show series tree only on series/subseries

--- src/ui/components/collection-home.vue
+++ src/ui/components/collection-home.vue
@@ -9,9 +9,10 @@
       <!--  slot="series-header">-->
       <!--</collection-heading>-->
 
-      <location-in-collection slot="location-in-collection"></location-in-collection>
+      <location-in-collection v-show="onlyForObject" slot="location-in-collection">
+      </location-in-collection>
 
-      <series-tree slot="collection-hierarchy" :series-tree="seriesTree">
+      <series-tree v-show="onlyForSeries" slot="collection-hierarchy" :series-tree="seriesTree">
       </series-tree>
 
     </collection-layout>
@@ -27,7 +28,7 @@ import { mapActions, mapState } from 'vuex'
 export default {
 
   // These props should be set by the router
-  props: ['collectionSlug', 'seriesSlug'],
+  props: ['collectionSlug', 'seriesSlug', 'objectSlug'],
 
   computed: {
     ...mapState([
@@ -38,6 +39,10 @@ export default {
 
     onlyForSeries () {
       return !!this.seriesSlug
+    },
+
+    onlyForObject () {
+      return !!this.objectSlug
     }
   },
 
