fedaff1b7efa340da3687d0dde0fb4f63ba3b42e
Rit Li <get@rit.li>
2017-08-03 13:12:07 -0700

    Add manifest viewer

---
+++ src/ui/components/manifest-viewer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>Navigate {{ label }}</h2>
+    <range-viewer
+       :manifest="manifest"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['manifest'],
+
+  components: {
+    'range-viewer': require('./range.vue')
+  },
+
+  computed: {
+    label () { return this.manifest.label }
+  }
+}
+</script>
+
+<style>
+
+h2 {
+  font-weight: bold;
+  padding-top: 0.5em;
+  margin-top: 2em;
+  border-top: 2px solid #3498db;
+}
+
+</style>
