d3ab5228e709de52d92874527b97dd9203fa22cf
Rit Li <get@rit.li>
2017-08-02 17:27:00 -0700

    clean up ranges

--- src/iso/ranger.js
+++ src/iso/ranger.js
@@ -1,37 +1,50 @@
 class Range {
-  constructor ({ db, label, subranges }) {
+  constructor ({ db, label, subranges, canvases }) {
     this.label = label
 
-    if (subranges) {
-      this.subranges = subranges.map((id) => {
-        let record = db[id]
-        return new Range({
-          db,
-          label: record['label'],
-          subranges: record['ranges']
-        })
-      })
-    }
+    if (subranges) this.subranges = subranges.map(id => mkRange(db, id))
 
     this.__class__ = this.constructor.name
   }
 }
 
-function parse(data) {
-  var db = data.reduce((acc, record) => {
-    id = record['@id']
-    acc[id] = record
+
+class Canvas {
+  constructor ({ label, thumbnail }) {
+    this.label = label
+    this.thumbnail = thumbnail
+
+    this.__class__ = this.constructor.name
+  }
+}
+
+
+function lookupDb (records, key) {
+  return records.reduce((acc, rec) => {
+    id = rec[key]
+    acc[id] = rec
     return acc
   }, {})
+}
 
-  let first = data[0]
-  let root = new Range({
+function mkRange(db, id) {
+  let record = db[id]
+  return new Range({
     db,
-    label: first['label'],
-    subranges: first['ranges']
+    label: record['label'],
+    subranges: record['ranges']
   })
+}
+
+function parse(manifest) {
+  var db = lookupDb(manifest.structures, '@id')
+  var canvasDb = lookupDb(manifest.sequences[0].canvases)
+
+  let first = manifest.structures[0]
+  let root = mkRange(db, first['@id'])
   return root
 }
+
 module.exports = {
   Range,
   parse
--- src/iso/ranger.spec.js
+++ src/iso/ranger.spec.js
@@ -3,7 +3,7 @@ var manifest = loadJsonFixture('gen_co_c.json')
 
 describe('parse', () => {
   it('returns the top level ranges', () => {
-    var root = parse(manifest.structures)
+    var root = parse(manifest)
 
     var labels = root.subranges.map(item => item.label)
     expect(labels).to.eql(['Papers', 'Color photograph'])
@@ -12,12 +12,12 @@ describe('parse', () => {
 
 describe('range', () => {
   it('has label attribute', () => {
-    var root = parse(manifest.structures)
+    var root = parse(manifest)
     expect(root.label).to.equal('General correspondence:  C')
   })
 
   it('has nested subranges', () => {
-    var root = parse(manifest.structures)
+    var root = parse(manifest)
     expect(root.subranges.length).to.equal(2)
 
     var papers = root.subranges[0]
