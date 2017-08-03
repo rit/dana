709377dd4fcecc77f93e5173d389652b423e8f87
Rit Li <get@rit.li>
2017-08-02 18:43:57 -0700

    Create canvas object

--- src/iso/ranger.js
+++ src/iso/ranger.js
@@ -1,8 +1,17 @@
 class Range {
-  constructor ({ rangeDb, label, subranges, canvases }) {
+  constructor ({ rangeDb, canvasDb, label, subranges, canvases }) {
     this.label = label
 
-    if (subranges) this.subranges = subranges.map(id => mkRange({ rangeDb, id }))
+    if (subranges) {
+      this.subranges = subranges.map(id => mkRange({ rangeDb, canvasDb, id }))
+    }
+
+    if (canvases) {
+      this.canvases = canvases.map((id) => {
+        var rec = canvasDb[id]
+        return new Canvas(rec)
+      })
+    }
 
     this.__class__ = this.constructor.name
   }
@@ -27,21 +36,24 @@ function lookupDb (records, key) {
   }, {})
 }
 
-function mkRange({ rangeDb, id }) {
+function mkRange({ rangeDb, canvasDb, id }) {
   let record = rangeDb[id]
+  let label = record['label']
   return new Range({
     rangeDb,
+    canvasDb,
     label: record['label'],
-    subranges: record['ranges']
+    subranges: record['ranges'],
+    canvases: record['canvases']
   })
 }
 
 function parse(manifest) {
   var rangeDb = lookupDb(manifest.structures, '@id')
-  var canvasDb = lookupDb(manifest.sequences[0].canvases)
+  var canvasDb = lookupDb(manifest.sequences[0].canvases, '@id')
 
-  let first = manifest.structures[0]
-  let root = mkRange({ rangeDb, id: first['@id'] })
+  let rootId = manifest.structures[0]['@id']
+  let root = mkRange({ rangeDb, canvasDb, id: rootId })
   return root
 }
 
--- src/iso/ranger.spec.js
+++ src/iso/ranger.spec.js
@@ -1,3 +1,4 @@
+var _ = require('lodash')
 var { Range, parse } = require('iso/ranger')
 var manifest = loadJsonFixture('gen_co_c.json')
 
@@ -14,6 +15,21 @@ describe('range', () => {
   it('has label attribute', () => {
     var root = parse(manifest)
     expect(root.label).to.equal('General correspondence:  C')
+  })
+
+  it('has canvas', () => {
+    var root = parse(manifest)
+    var papers = root.subranges[0]
+    var ca = papers.subranges[0]
+    var canvases = ca.canvases
+    expect(ca.label).to.eql('CA')
+    expect(canvases[0].label).to.equal('Image 001')
+
+    var imageUrl = '//media.getty.edu/iiif/research/archives/2011m30_ref234462_3rn_FL2292185_thumb/full/full/0/default.jpg'
+
+    expect(canvases[0].thumbnail['@id']).to.contain(imageUrl)
+
+    expect(_.last(canvases).label).to.equal('Image 054')
   })
 
   it('has nested subranges', () => {
