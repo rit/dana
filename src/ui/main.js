22b61c7475c55ce4f5b82be0be098f8b840057d1
Rit Li <get@rit.li>
2017-08-08 22:37:24 -0700

    initStore will install Vuex

--- src/ui/main.js
+++ src/ui/main.js
@@ -1,7 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuex from 'vuex'
 import Router from 'vue-router'
 
 import ElementUI from 'element-ui'
@@ -15,7 +14,6 @@ Vue.use(Router)
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
-Vue.use(Vuex)
 var store = initStore(Vue)
 var router = new Router({ routes })
 
