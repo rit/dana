202ba2c314096c271d57f9b66e7eeeddc486a866
Rit Li <get@rit.li>
2017-08-09 13:09:11 -0700

    Pane is active by currentName

--- src/ui/components/range-viewer.spec.js
+++ src/ui/components/range-viewer.spec.js
@@ -8,6 +8,14 @@ Vue.component('range-viewer', RangeViewer)
 
 describe('range-viewer', () => {
   let vm
+  let template = `
+    <range-viewer current-name="Dragon">
+      <el-tab-pane ref="dragon" name="Dragon"> <p>Dragon</p> </el-tab-pane>
+      <el-tab-pane ref="tiger" name="Tiger"> <p>Tiger</p> </el-tab-pane>
+      <el-tab-pane ref="snake" name="Snake"> <p>Snake</p> </el-tab-pane>
+      <el-tab-pane ref="ox" name="Ox"> <p>Ox</p> </el-tab-pane>
+    </range-viewer>
+  `
 
   beforeEach(() => {
   });
@@ -23,22 +31,23 @@ describe('range-viewer', () => {
       </range-viewer>
     `
     vm = vmInit({ template })
-    expect(vm.$el.children[0].innerHTML.trim()).to.equal('Dragon')
+    expect(vm.$el.innerHTML).to.contain('Dragon')
+  })
+
+  it('shows headers', (done) => {
+    vm = vmInit({ template }, null, true)
+    swing(vm.$nextTick(), done, () => {
+      expect(vm.$el.querySelector('header').innerText)
+        .to.equal('Dragon\nTiger\n\Snake\nOx\n')
+    })
+
   })
 
   it('acts like an accordion', (done) => {
-    const template = `
-      <range-viewer current-name="Dragon">
-        <el-tab-pane name="Dragon">
-          <p>Dragon</p>
-        </el-tab-pane>
-      </range-viewer>
-    `
-
     vm = vmInit({ template }, null, true)
     swing(vm.$nextTick(), done, () => {
-      expect(vm.$el.querySelector('p').innerHTML).to.equal('Dragon')
-      expect(isVisible(vm.$el.children[0])).to.be.true
+      expect(isVisible(vm.$refs.dragon.$el)).to.be.true
+      expect(isVisible(vm.$refs.tiger.$el)).to.be.false
     })
   })
 })
