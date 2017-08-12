be3f513d2a4fe6c0de9ab147882f70063e0ccaec
Rit Li <get@rit.li>
2017-08-12 09:39:10 -0700

    Show thumbnails for subrange

---
+++ src/ui/components/subrange.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="subrange-details">
+    <h3>
+      {{ range.label }}
+    </h3>
+    <p>
+      {{ range.description }}
+    </p>
+
+    <thumbnail-viewer :canvases="range.canvases"></thumbnail-viewer>
+
+    <!--<div class="subrange-details" v-for="sub in range.subranges">-->
+    <!--  <subrange :range="sub"></subrange>-->
+    <!--</div>-->
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'subrange',
+  props: ['range'],
+
+  components: {
+    'thumbnail-viewer': require('./thumbnail-viewer.vue')
+  },
+
+  computed: {
+    isLeaf () {
+      return !this.range.subranges
+    }
+  }
+}
+</script>
