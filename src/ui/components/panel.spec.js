867f34232824db83f6dfaac88d1c8188907286f7
Rit Li <get@rit.li>
2017-08-29 12:17:54 -0700

    Add hide/show

--- src/ui/components/panel.spec.js
+++ src/ui/components/panel.spec.js
@@ -1,6 +1,6 @@
 import swing from 'icemaker-swing'
 
-import { vmInit } from '@ui/testing'
+import { vmInit, isVisible } from '@ui/testing'
 
 import Vue from 'vue'
 import Panel from '@ui/components/panel'
@@ -35,5 +35,48 @@ describe('panel', () => {
 
     expect(vm.$el.innerText.trim()).to.equal('Dragon')
     expect(methods.addPanel).to.have.been.called
+  })
+
+  it('can hide its content', () => {
+    var template = `
+      <div>
+        <panel ref="dragon" title="Dragon"><p>Dragon</p></panel>
+      </div>
+    `
+    var methods = {
+      addPanel: sinon.spy()
+    }
+    var Parent =  Vue.extend({ template, methods })
+    var vm = vmInit(Parent, {}, true)
+    var panelVm = vm.$refs.dragon
+
+    expect(isVisible(panelVm.$el)).to.be.false
+  })
+
+  it('can show its content', () => {
+    var template = `
+      <div>
+        <panel ref="dragon" title="Dragon" :activePanel="currentPanel"><p>Dragon</p></panel>
+      </div>
+    `
+    var methods = {
+      addPanel: sinon.spy()
+    }
+    var data = () => {
+      return {
+        'currentPanel': null
+      }
+    }
+
+    var Parent =  Vue.extend({ template, methods, data })
+    var vm = vmInit(Parent, {}, true)
+    var panelVm = vm.$refs.dragon
+
+    expect(isVisible(panelVm.$el)).to.be.false
+
+    vm.currentPanel = 'Dragon'
+    vm.$nextTick(() => {
+      expect(isVisible(panelVm.$el)).to.be.true
+    })
   })
 })
--- src/ui/components/panel.vue
+++ src/ui/components/panel.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="panel">
+  <div class="panel" v-show="isActive">
     <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
+  props: ['title', 'activePanel'],
+
+  computed: {
+    isActive () {
+      return this.title == this.activePanel
+    }
+  },
+
   mounted () {
     this.$parent.addPanel(this)
   }
