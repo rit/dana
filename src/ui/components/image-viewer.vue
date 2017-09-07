babf699bf8d0cd5bf13edd343fcbbbf15033d592
Rit Li <get@rit.li>
2017-09-06 21:39:31 -0700

    Send data to Mirador via querystring

--- src/ui/components/image-viewer.vue
+++ src/ui/components/image-viewer.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="image-viewer">
-    <h4>Image Viewer Placeholder</h4>
+    <iframe
+      src="/static/mirador/index.html?manifest=szeeman"
+      title="Mirador"
+      allowfullscreen="true"
+      webkitallowfullscreen="true"
+      mozallowfullscreen="true">
+    </iframe>
+
   </div>
 </template>
 
 <script>
 
 export default {
-
 }
 </script>
 
@@ -15,11 +21,15 @@ export default {
 <style scoped>
   div {
     display: flex;
-    height: 20em;
     justify-content: center;
     align-items: center;
     border: 1px solid silver;
     margin-top: 1em;
     margin-bottom: 2em;
+  }
+
+  iframe {
+    width: 100%;
+    height: 40em;
   }
 </style>
